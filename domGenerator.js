9879be5ebca003a99be3aeeedc3aa38d129f6cd7
Owen <owenyangg@gmail.com>
2016-04-06 22:09:28 -1000

    adds POST, PUT, GET and DELETE

--- domGenerator.js
+++ domGenerator.js
@@ -7,6 +7,7 @@ function DomGen(){
            "<head>" + "\n" +
            "  <meta charset='UTF-8'>" + "\n" +
            "  <title>" + title + "</title>" + "\n" +
+           "  <link rel='stylesheet' href='/css/styles.css'>" + "\n" +
            "</head>" + "\n" +
            "<body>" + "\n\n";
 
@@ -18,17 +19,24 @@ function DomGen(){
 
   }
 
-  function makeElement(element, innerHTML){
-    return "  <" + element + ">" + innerHTML + "</" + element + ">\n";
+  function makeElement(element, innerHTML, attribute){
+    if (attribute !== undefined){
+      return "<" + element + " " + attribute.name + "=" + "'" + attribute.value + "'" + ">" + innerHTML + "</" + element + ">";
+    }
+
+    return "  <" + element + ">" + innerHTML + "</" + element + ">";
   }
+
 
   function elementsTemplate(name, symbol, number, des){
     var elementsHTML = "";
 
     elementsHTML += makeTop(name);
-    elementsHTML += makeElement('h1',name);
-    elementsHTML += makeElement('h2', symbol + " " + number);
-    elementsHTML += makeElement('p', des);
+    elementsHTML += makeElement('h1',name) + "\n";
+    elementsHTML += makeElement('h2', symbol) + "\n";
+    elementsHTML += makeElement('h3', "Atomic number " + number) + "\n";
+    elementsHTML += makeElement('p', des) + "\n";
+    elementsHTML += makeElement('p', makeElement('a', "back", {name: 'href', value: '/'})) + "\n";
     elementsHTML += makeClose();
 
     return elementsHTML;
--- server.js
+++ server.js
@@ -3,6 +3,10 @@ var fs = require('fs');
 var domGen = require('./domGenerator.js');
 var toHTML = domGen();
 
+function replaceAt(string, index, newtext) {
+  return string.substring(0, index) + newtext + string.substring(index + 1);
+}
+
 var server = http.createServer();
 
 server.on('request',function(request,response){
@@ -14,11 +18,8 @@ server.on('request',function(request,response){
   var cedObj = {};
   var message = "";
 
-  console.log('method', method);
-  console.log('url', url);
 
   request.on('data', function(data){
-    console.log('data.toString()', data.toString());
     message += data;
   });
 
@@ -31,44 +32,148 @@ server.on('request',function(request,response){
         var key = element.slice(0, element.indexOf("="));
         var value = element.slice(element.indexOf("=") + 1, element.length);
 
-        value = value.split("+").join(" ");
-
+        value = value.replace(/\+/g, " ");
+        value = value.replace(/%2C/g, ",");
         cedObj[key] = value;
+
       });
 
       var htmlDoc = toHTML.elementsTemplate(cedObj.elementName, cedObj.elementSymbol, cedObj.elementAtomicNumber, cedObj.elementDescription);
 
-      var newFile = fs.createWriteStream("./public/" + cedObj.elementName + ".html");
-      newFile.write(htmlDoc);
-      console.log("end of request");
+      fs.stat("./public/" + cedObj.elementName + ".html", function(error){
+        if(error){
+          var newFile = fs.createWriteStream("./public/" + cedObj.elementName + ".html");
+          newFile.write(htmlDoc);
+
+
+          fs.readFile("./public/index.html", 'utf8', function(error, data){
+            var indexFile = data;
+            var newIndex = fs.createWriteStream("./public/index.html");
+            var prepend = indexFile.indexOf("prepend") + 9;
+            newIndex.write(replaceAt(indexFile, prepend, "\n    <li><a href=\'/" + cedObj.elementName + ".html\'>" + cedObj.elementName + "</a>" + "</li>\n"));
+          });
+        }
+        else {
+          console.log("file already exists");
+        }
+      });
+    });
+
+    var responseBody = {
+      "success" : true
+    };
+
+    response.writeHead(200, {
+      'Content-Type': 'application/json',
+    });
+
+    response.write(JSON.stringify(responseBody));
+    response.end();
+
+  }
+
+
+  if (method === "GET"){
+
+    if (url === "/" || url === " "){
+      url = "./index.html";
+    }
+
+    fs.stat("./public/" + url, function(error){
+
+      if(error){
+        fs.readFile("./public/404.html", 'utf8', function(error, data){
+
+        response.write(data.toString());
+        response.end();
+        });
+      }
+
+      else{
+        fs.readFile("./public/" + url, 'utf8', function(error, data){
+
+        response.write(data.toString());
+        response.end();
+        });
+      }
+
     });
 
   }
 
-  else if (method === "GET"){
+  if (method === "PUT"){
 
-    request.on('end', function(data){
+    request.on('end', function(){
+      var newFileContents = message.toString().split("&");
 
+      newFileContents = newFileContents.forEach(function(element){
+        var key = element.slice(0, element.indexOf("="));
+        var value = element.slice(element.indexOf("=") + 1, element.length);
+
+        value = value.replace(/\+/g, " ");
+        value = value.replace(/%2C/g, ",");
+        cedObj[key] = value;
+
+      });
+
+      var htmlDoc = toHTML.elementsTemplate(cedObj.elementName, cedObj.elementSymbol, cedObj.elementAtomicNumber, cedObj.elementDescription);
+
+      fs.stat("./public/" + url, function(error){
+        if(error){
+          console.log("file does not exist");
+        }
+        else {
+          var newFile = fs.createWriteStream("./public/" + url);
+          newFile.write(htmlDoc);
+          }
+      });
     });
 
+    var responseBdy = {
+      "success" : true
+    };
+
+    response.writeHead(200, {
+      'Content-Type': 'application/json',
+    });
+
+    response.write(JSON.stringify(responseBdy));
+    response.end();
 
   }
 
+  if (method === "DELETE" ){
+
+    var responsebody = {
+      "success" : false
+    };
+
+    if(url.indexOf('index') === -1){
+      fs.unlink("./public/" + url, function(error){
+
+      });
+
+      responsebody.success = true;
+    }
+
+    else {
+      responsebody.success = false;
+      responsebody.error = "resource " + url + " does not exist";
+    }
+
+
+    response.writeHead(200, {
+      'Content-Type': 'application/json',
+    });
+
+    response.write(JSON.stringify(responsebody));
+    response.end();
+  }
 
   request.on('error', function(error){
-    console.log(error.stack);
+    console.log('error', error);
   });
 
-  response.writeHead(200, {
-    'Content-Type': 'application/json',
-  });
-
-  var responseBody = {
-    "success" : true
-  };
-
-  response.write(JSON.stringify(responseBody));
-  response.end();
 
 });
 
