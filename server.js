e86d8f73608edc1d6636ba32660fc6639ad939eb
Owen <owenyangg@gmail.com>
2016-04-07 14:56:19 -1000

    finishes error handling and authentication

--- server.js
+++ server.js
@@ -13,167 +13,321 @@ server.on('request',function(request,response){
   var method = request.method;
   var url = request.url;
   var headers = request.headers;
-  var userAgent = headers['user-agent'];
 
   var cedObj = {};
   var message = "";
-
-
   request.on('data', function(data){
     message += data;
   });
 
-  if (method === "POST" && url === "/elements"){
+  var acceptableFields = ["elementName", "elementSymbol", "elementAtomicNumber", "elementDescription"];
+  var fieldIsUndefined;
 
-    request.on('end', function(){
-      var newFileContents = message.toString().split("&");
-
-      newFileContents = newFileContents.forEach(function(element){
-        var key = element.slice(0, element.indexOf("="));
-        var value = element.slice(element.indexOf("=") + 1, element.length);
-
-        value = value.replace(/\+/g, " ");
-        value = value.replace(/%2C/g, ",");
-        cedObj[key] = value;
-
-      });
-
-      var htmlDoc = toHTML.elementsTemplate(cedObj.elementName, cedObj.elementSymbol, cedObj.elementAtomicNumber, cedObj.elementDescription);
-
-      fs.stat("./public/" + cedObj.elementName + ".html", function(error){
-        if(error){
-          var newFile = fs.createWriteStream("./public/" + cedObj.elementName + ".html");
-          newFile.write(htmlDoc);
-
-
-          fs.readFile("./public/index.html", 'utf8', function(error, data){
-            var indexFile = data;
-            var newIndex = fs.createWriteStream("./public/index.html");
-            var prepend = indexFile.indexOf("prepend") + 9;
-            newIndex.write(replaceAt(indexFile, prepend, "\n    <li><a href=\'/" + cedObj.elementName + ".html\'>" + cedObj.elementName + "</a>" + "</li>\n"));
-          });
-        }
-        else {
-          console.log("file already exists");
-        }
-      });
-    });
-
-    var responseBody = {
+  var responseBody = {
       "success" : true
-    };
+  };
 
-    response.writeHead(200, {
-      'Content-Type': 'application/json',
+  if(!headers.authorization){
+
+    console.log('headers.authorization', headers.authorization);
+
+    response.writeHead(401, {
+      'Content-Type': 'text/html',
+      'WWW-Authenticate' : "Basic realm='Secure Area'"
     });
 
-    response.write(JSON.stringify(responseBody));
+    response.write("<html><body>Not Authorized</body></html>");
     response.end();
-
   }
 
+  else {
 
-  if (method === "GET"){
+    var encodedString = headers.authorization;
+    encodedString = encodedString.substring(encodedString.indexOf("Basic ") + 6);
+    console.log('encodedString', encodedString);
 
-    if (url === "/" || url === " "){
-      url = "./index.html";
+    var base64Buffer = new Buffer(encodedString, 'base64');
+    var decodedString = base64Buffer.toString();
+    decodedString = decodedString.split(":");
+
+
+    if(method !== 'GET' && decodedString[1] !== users[decodedString[0]]){
+      response.writeHead(401, {
+        'Content-Type': 'text/html',
+      });
+
+      response.write("<html><body>Invalid Authentication Credentials</body></html>");
+      response.end();
     }
 
-    fs.stat("./public/" + url, function(error){
 
-      if(error){
-        fs.readFile("./public/404.html", 'utf8', function(error, data){
 
-        response.write(data.toString());
-        response.end();
+    else if (method === "POST" && url === "/elements"){
+      fieldIsUndefined = false;
+
+      request.on('end', function(){
+        var newFileContents = message.toString().split("&");
+
+        newFileContents = newFileContents.forEach(function(element){
+          var key = element.slice(0, element.indexOf("="));
+          var value = element.slice(element.indexOf("=") + 1, element.length);
+
+          value = value.replace(/\+/g, " ");
+          value = value.replace(/%2C/g, ",");
+          cedObj[key] = value;
+
         });
-      }
 
-      else{
-        fs.readFile("./public/" + url, 'utf8', function(error, data){
 
-        response.write(data.toString());
-        response.end();
+        for (var key in Object.keys(cedObj)){
+          if (acceptableFields.indexOf(Object.keys(cedObj)[key]) === -1){
+            fieldIsUndefined = true;
+          }
+        }
+
+        if (fieldIsUndefined === false){
+
+        var htmlDoc = toHTML.elementsTemplate(cedObj.elementName, cedObj.elementSymbol, cedObj.elementAtomicNumber, cedObj.elementDescription);
+
+        fs.stat("./public/" + cedObj.elementName + ".html", function(error){
+          if(error){
+            var newFile = fs.createWriteStream("./public/" + cedObj.elementName + ".html");
+            newFile.write(htmlDoc);
+
+            fs.readFile("./public/index.html", 'utf8', function(error, data){
+              var indexFile = data;
+              var newIndex = fs.createWriteStream("./public/index.html");
+              var prepend = indexFile.indexOf("prepend") + 9;
+              newIndex.write(replaceAt(indexFile, prepend, "\n    <li><a href=\'/" + cedObj.elementName + ".html\'>" + cedObj.elementName + "</a>" + "</li>\n"));
+            });
+
+            response.writeHead(200, {
+              'Content-Type': 'application/json',
+            });
+
+            response.write(JSON.stringify(responseBody));
+            response.end();
+          }
+          else {
+
+            response.writeHead(409, {
+              'Content-Type': 'application/json',
+            });
+
+            responseBody = {
+              "success" : false,
+              "error" : "File already Exists"
+            };
+
+            response.write(JSON.stringify(responseBody));
+            response.end();
+          }
         });
-      }
 
-    });
+        }
 
-  }
+        else {
+            response.writeHead(404, {
+              'Content-Type': 'application/json',
+            });
 
-  if (method === "PUT"){
+            responseBody = {
+              "success" : false,
+              "error" : "Incorrect field data entered in body"
+            };
 
-    request.on('end', function(){
-      var newFileContents = message.toString().split("&");
-
-      newFileContents = newFileContents.forEach(function(element){
-        var key = element.slice(0, element.indexOf("="));
-        var value = element.slice(element.indexOf("=") + 1, element.length);
-
-        value = value.replace(/\+/g, " ");
-        value = value.replace(/%2C/g, ",");
-        cedObj[key] = value;
+            response.write(JSON.stringify(responseBody));
+            response.end();
+        }
 
       });
 
-      var htmlDoc = toHTML.elementsTemplate(cedObj.elementName, cedObj.elementSymbol, cedObj.elementAtomicNumber, cedObj.elementDescription);
+    } //End of POST
+
+
+    else if (method === "GET"){
+
+      if (url === "/" || url === " "){
+        url = "./index.html";
+      }
 
       fs.stat("./public/" + url, function(error){
+
         if(error){
-          console.log("file does not exist");
+
+          fs.readFile("./public/404.html", 'utf8', function(error, data){
+
+          response.write(data.toString());
+          response.end();
+          });
         }
-        else {
-          var newFile = fs.createWriteStream("./public/" + url);
-          newFile.write(htmlDoc);
+
+        else{
+
+          fs.readFile("./public/" + url, 'utf8', function(error, data){
+
+          response.write(data.toString());
+          response.end();
+          });
+        }
+
+      });
+
+    } //End of GET
+
+    else if (method === "PUT"){
+
+      fieldIsUndefined = false;
+
+      request.on('end', function(){
+        var newFileContents = message.toString().split("&");
+
+        newFileContents = newFileContents.forEach(function(element){
+          var key = element.slice(0, element.indexOf("="));
+          var value = element.slice(element.indexOf("=") + 1, element.length);
+
+          value = value.replace(/\+/g, " ");
+          value = value.replace(/%2C/g, ",");
+          cedObj[key] = value;
+
+        });
+
+        for (var key in Object.keys(cedObj)){
+          if (acceptableFields.indexOf(Object.keys(cedObj)[key]) === -1){
+            fieldIsUndefined = true;
           }
+        }
+
+        if (fieldIsUndefined === false){
+
+          var htmlDoc = toHTML.elementsTemplate(cedObj.elementName, cedObj.elementSymbol, cedObj.elementAtomicNumber, cedObj.elementDescription);
+
+          fs.stat("./public/" + url, function(error){
+            if(error){
+              response.writeHead(404, {
+                'Content-Type': 'application/json',
+              });
+
+              responseBody = {
+                "success" : false,
+                "error" : "File Not Found"
+              };
+
+              response.write(JSON.stringify(responseBody));
+              response.end();
+            }
+            else {
+              var newFile = fs.createWriteStream("./public/" + url);
+              newFile.write(htmlDoc);
+
+              response.writeHead(200, {
+                'Content-Type': 'application/json',
+              });
+
+              response.write(JSON.stringify(responseBody));
+              response.end();
+              }
+          });
+
+        }
+
+        else {
+          response.writeHead(404, {
+            'Content-Type': 'application/json',
+          });
+
+          responseBody = {
+            "success" : false,
+            "error" : "Incorrect Field Titles"
+          };
+
+          response.write(JSON.stringify(responseBody));
+          response.end();
+        }
+
       });
-    });
 
-    var responseBdy = {
-      "success" : true
-    };
+    } //end of PUT
 
-    response.writeHead(200, {
-      'Content-Type': 'application/json',
-    });
+    else if (method === "DELETE" ){
 
-    response.write(JSON.stringify(responseBdy));
-    response.end();
+      var code = 200;
 
-  }
+      responseBody = {
+        "success" : false
+      };
 
-  if (method === "DELETE" ){
+      if(url.indexOf('index') === -1 && url !== "/" && url.indexOf('404') === -1 ){
+        fs.unlink("./public/" + url, function(error){
 
-    var responsebody = {
-      "success" : false
-    };
+          if(error){
+            code = 404;
+            response.writeHead(code, {
+              'Content-Type': 'application/json',
+            });
+            responseBody.success = false;
+            response.write(JSON.stringify(responseBody));
+            response.end();
+          }
 
-    if(url.indexOf('index') === -1){
-      fs.unlink("./public/" + url, function(error){
+          else {
+            code = 200;
+            response.writeHead(code, {
+              'Content-Type': 'application/json',
+            });
 
+            responseBody.success = true;
+            response.write(JSON.stringify(responseBody));
+            response.end();
+          }
+
+        });
+
+      }
+
+      else if(url.indexOf('index') !== -1 || url === "/" || url.indexOf('404') !== -1 ){
+
+        code = 409;
+        response.writeHead(code, {
+          'Content-Type': 'application/json',
+        });
+
+        responseBody.success = false;
+        responseBody.error = "cannot delete " + url;
+        response.write(JSON.stringify(responseBody));
+        response.end();
+      }
+
+      else {
+
+        code = 404;
+        response.writeHead(code, {
+          'Content-Type': 'application/json',
+        });
+
+        responseBody.success = false;
+        responseBody.error = "resource " + url + " does not exist";
+        response.write(JSON.stringify(responseBody));
+        response.end();
+      }
+
+    } //end of DELETE
+
+    request.on('error', function(error){
+      response.writeHead(400, {
+        'Content-Type': 'application/json',
       });
 
-      responsebody.success = true;
-    }
+      responseBody = {
+        "success" : false,
+        "error" : "Bad Request"
+      };
 
-    else {
-      responsebody.success = false;
-      responsebody.error = "resource " + url + " does not exist";
-    }
-
-
-    response.writeHead(200, {
-      'Content-Type': 'application/json',
+      response.write(JSON.stringify(responseBody));
+      response.end();
     });
 
-    response.write(JSON.stringify(responsebody));
-    response.end();
-  }
-
-  request.on('error', function(error){
-    console.log('error', error);
-  });
-
+  }//ends authorization IF
 
 });
 
